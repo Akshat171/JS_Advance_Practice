15c90119331fbef44fb43747cde3c6d767f87174
Akshat <akshatjangir1710@gmail.com>
2023-10-13 19:37:54 +0530

    updated

---
+++ Part_2/DOM/intro_to_dom.js
@@ -0,0 +1,10 @@
+// Document object model
+document.getElementById("firstHeading");
+// output--> <h1></h1>
+
+//core process-->
+document.getElementById("firstHading").innerHTML = "<h1>chai and code</h1>";
+// called dom manipulation
+//page manipulation
+
+// DOM--> manipulating HTML page using Javascript
